5e5bf1e63ec51173eb5d29c522dbca54f48635ae
2022bcd49naman <naman22bcd49@iiitkottayam.ac.in>
2026-01-23 21:06:18 +0530

    init

---
+++ app.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import numpy as np
+
+model = joblib.load("model.pkl")
+
+app = FastAPI(title="Wine Quality Prediction API")
+
+class WineInput(BaseModel):
+    fixed_acidity: float
+    volatile_acidity: float
+    citric_acid: float
+    residual_sugar: float
+    chlorides: float
+    free_sulfur_dioxide: float
+    total_sulfur_dioxide: float
+    density: float
+    pH: float
+    sulphates: float
+    alcohol: float
+
+
+@app.post("/predict")
+def predict_wine_quality(data: WineInput):
+    features = np.array([[ 
+    data.fixed_acidity,
+    data.volatile_acidity,
+    data.citric_acid,
+    data.residual_sugar,
+    data.chlorides,
+    data.free_sulfur_dioxide,
+    data.total_sulfur_dioxide,
+    data.density,
+    data.pH,
+    data.sulphates,
+    data.alcohol
+]])
+
+
+    prediction = model.predict(features)[0]
+
+    return {
+        "name": "Naman Omar",          
+        "roll_no": "2022BCD0049",
+        "wine_quality": int(round(prediction))
+    }
